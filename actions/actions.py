45689924c287c8ca58e56b7d07e71e621c8c5097
IT <icode67658@gmail.com>
2023-12-20 11:51:48 +0800

    implemented ticket creation

--- actions/actions.py
+++ actions/actions.py
@@ -2,10 +2,25 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.events import UserUtteranceReverted, SlotSet
 
 import requests
 
+# api_endpoint = "http://localhost:3210/"
+api_endpoint = "https://fyp-server-b4yk.onrender.com/"
+
+class ActionDefaultFallback(Action):
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(response="utter_rephrase")
+
+        return [UserUtteranceReverted()]
 
 class ActionTwoStageFallback(Action):
 
@@ -16,35 +31,82 @@ class ActionTwoStageFallback(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(response="utter_ask_rephrase")
+        dispatcher.utter_message(response="utter_rephrase")
 
         return [UserUtteranceReverted()]
 
-class ActionFAQ(Action):
+
+class ActionGetKnowLedges(Action):
 
     def name(self) -> Text:
-        return "action_faq"
+        return "action_get_knowledges"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        query = next(tracker.get_latest_entity_values("query"), None)
+        knowledge = next(tracker.get_latest_entity_values("knowledge"), None)
 
-        if query != None:
-            # api_endpoint = f'https://fyp-server-b4yk.onrender.com/api/knowledges/search?search={query}'
-            api_endpoint = f'http://localhost:3210/api/knowledges/search?search={query}'
-            res = requests.get(api_endpoint)
+        if knowledge != None:
+            url = f'{api_endpoint}api/knowledges/search?search={knowledge}'
+            res = requests.get(url)
 
             results = res.json().get('data');
 
             if len(results) > 0:
-                dispatcher.utter_message(text=f"Here is the information regarding {query}.\n")
+                dispatcher.utter_message(text=f"Here is the information regarding {knowledge}.\n")
                 dispatcher.utter_message(text=results[0]['description'])
             else:
-                dispatcher.utter_message(text=f"I can't find the information of it. Can you try something else?")
+                dispatcher.utter_message(text="I can't find the information of it. Can you try something else?")
         else:
-             dispatcher.utter_message(text=f"I can't get it. Can you rephrase it?")
+             dispatcher.utter_message(text="I can't get it. Can you rephrase it?")
+       
 
         return []
 
+
+
+class SubmitTicketForm(Action):
+
+    def name(self) -> Text:
+        return "submit_ticket_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        to_submit = {
+            "studentId": tracker.sender_id,
+            "subject": tracker.get_slot("ticket_subject"),
+            "detail":  tracker.get_slot("ticket_detail")
+        }
+
+        try:
+            url = f"{api_endpoint}api/tickets"
+            res = requests.post(url, json=to_submit)
+            res.raise_for_status()
+            dispatcher.utter_message(text="The ticket is created!")
+            dispatcher.utter_message(text="You may check your ticket in your profile page.")
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            
+            results = res.json().get('message')
+            if results is not None:
+                dispatcher.utter_message(text=results)
+            else:
+                dispatcher.utter_message(text="Network error occured. Please try again later.")
+         
+
+        return [SlotSet("ticket_subject", None), SlotSet("ticket_detail", None)]
+
+class CancelTicketForm(Action):
+
+    def name(self) -> Text:
+        return "cancel_ticket_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        return [SlotSet("ticket_subject", None), SlotSet("ticket_detail", None)]
