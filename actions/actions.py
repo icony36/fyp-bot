e56d911c8ce2a642c55f002c2e8bf91927b6500c
IT <icode67658@gmail.com>
2024-02-19 10:20:35 +0800

    updates

--- actions/actions.py
+++ actions/actions.py
@@ -29,9 +29,10 @@ def ask_google(dispatcher, text):
 
         responses = chat.send_message(text, stream=True)
 
-        for chunk in responses:
-            reply_text += chunk.text
-        
+        for chunk in responses: 
+            new_text = chunk.text.replace("\n\n", "\n \n") 
+            reply_text += new_text
+
         dispatcher.utter_message(text=reply_text)
     except Exception as e:
         print(e)
@@ -48,7 +49,7 @@ class ActionDefaultFallback(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         text = tracker.latest_message['text']
-        
+
         # if (detect(text) != "en"):
         #     dispatcher.utter_message(response="utter_english_only")
         #     return [UserUtteranceReverted()]
@@ -58,22 +59,94 @@ class ActionDefaultFallback(Action):
 
         ask_google(dispatcher, text)
 
-        # try:
-        #     reply_text = ""
-
-        #     responses = chat.send_message(text, stream=True)
-
-        #     for chunk in responses:
-        #         reply_text += chunk.text
-            
-        #     dispatcher.utter_message(text=reply_text)
-        # except Exception as e:
-        #     print(e)
-            
-        #     dispatcher.utter_message(response="utter_cant_answer")
-
-
         return [UserUtteranceReverted()]
+
+class ActionGetTimetable(Action):
+    def name(self) -> Text:
+        return "action_get_timetable"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        url = f'{server_endpoint}api/student-profiles/by-user/{tracker.sender_id}'
+        res = requests.get(url)
+
+        results = res.json().get('data');
+
+        if results is not None:
+            dispatcher.utter_message(text="Here is your timetable.")
+
+            reply_str = ""
+
+            for time in results["timetable"]:
+                temp = f'{time["moduleId"]} {time["moduleName"]} {time["lessonType"]} {time["location"]} {time["date"]} {time["time"]} \n'
+                reply_str += temp + " \n"
+            
+            dispatcher.utter_message(text=reply_str)
+            dispatcher.utter_message(text="You may also check your timetable on your profile page.")
+        else:
+            dispatcher.utter_message(text="Can't retrieve your student id")
+
+        return []
+
+class ActionGetFee(Action):
+    def name(self) -> Text:
+        return "action_get_fee"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        url = f'{server_endpoint}api/student-profiles/by-user/{tracker.sender_id}'
+        res = requests.get(url)
+
+        results = res.json().get('data');
+
+        if results != None:
+            dispatcher.utter_message(text="Here is your outstanding fee.")
+            dispatcher.utter_message(text=f'$ {results["outstandingFee"]}')
+            dispatcher.utter_message(text="You may also check your outstanding fee on your profile page.")
+        else:
+            dispatcher.utter_message(text="Can't retrieve your student id")
+
+        return []
+
+class ActionGetCareerPath(Action):
+    def name(self) -> Text:
+        return "action_get_career_path"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        url = f'{server_endpoint}api/student-profiles/by-user/{tracker.sender_id}'
+        res = requests.get(url)
+
+        results = res.json().get('data');
+
+        course = next(tracker.get_latest_entity_values("course"), None)
+
+        text = tracker.latest_message['text']
+
+        if results is not None and course is None:
+            text += f' {results["course"]}'
+
+        ask_google(dispatcher, text)
+
+        return []
+
+class ActionGetStudentId(Action):
+    def name(self) -> Text:
+        return "action_get_student_id"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(text=f"Your student ID is {tracker.sender_id}.")
+
+        return []
 
 
 class ActionGetKnowLedges(Action):
@@ -87,7 +160,7 @@ class ActionGetKnowLedges(Action):
 
         knowledge = next(tracker.get_latest_entity_values("knowledge"), None)
 
-        if knowledge != None:
+        if knowledge is not None:
             url = f'{server_endpoint}api/knowledges/search?search={knowledge}'
             res = requests.get(url)
 
@@ -159,8 +232,24 @@ class ActionMapDirection(Action):
         
         link = "https://map-viewer.situm.com/?apikey=b48aeb58c2a605532de776ee5dedfefc778ca5b242f3d1c56986044c2baddb93&domain=&lng=en&buildingid=15400&floorid=49585"
      
-        dispatcher.utter_message(text="Sure, you may use the following link to get the direction to your destination.")
+        dispatcher.utter_message(text="You may use the following link to get the direction to your destination.")
         dispatcher.utter_message(text=link)
         dispatcher.utter_message(text="Set your starting point and destination to get the direction.")
         
+        return []
+
+class ActionCampusDirection(Action):
+    def name(self) -> Text:
+        return "action_campus_direction"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+
+        text = tracker.latest_message['text'] + ' SIM GE Singapore'
+
+        dispatcher.utter_message(text='The school is  located at 461 Clementi Rd, Singapore 599491.')
+        ask_google(dispatcher, text)
+
         return []
